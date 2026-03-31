2943e7bcfe80d9807f04d509621d6d138eeb1824
Eeritvan <144259545+Eeritvan@users.noreply.github.com>
2026-03-31 15:44:45 +0300

    part 2: updated app to use ts instea of js

---
+++ tdd-mooc-tetris/src/Board.ts
@@ -0,0 +1,14 @@
+export class Board {
+  width: number;
+  height: number;
+
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+  }
+
+  toString() {
+    return `...\n...\n...\n`
+
+  }
+}
