2af996aa5baca87158d8325a3e105ab176462615
Eeritvan <144259545+Eeritvan@users.noreply.github.com>
2026-03-31 16:11:00 +0300

    part 2: added drop function to board

--- tdd-mooc-tetris/src/Board.ts
+++ tdd-mooc-tetris/src/Board.ts
@@ -1,14 +1,23 @@
 export class Board {
   width: number;
   height: number;
+  private dropping: string | null;
+  private board: string[][];
 
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
+    this.dropping = null;
+    this.board = Array.from({ length: height }, () => Array(width).fill("."));
   }
 
   toString() {
-    return `...\n...\n...\n`
+    return this.board.map((x) => x.join("")).join("\n") + "\n"
+  }
 
+  drop(shape: string) {
+    this.dropping = shape
+    this.board[0][1] = this.dropping
+    console.log(this.board)
   }
 }
