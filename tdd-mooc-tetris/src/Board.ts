20a17e00c60dc7aa606b0095ace053c4f6da18d3
Eeritvan <144259545+Eeritvan@users.noreply.github.com>
2026-04-01 12:19:51 +0300

    part 2: refactor drop function

--- tdd-mooc-tetris/src/Board.ts
+++ tdd-mooc-tetris/src/Board.ts
@@ -2,12 +2,14 @@ export class Board {
   width: number;
   height: number;
   private dropping: string | null;
+  private position: { x: number, y: number } | null;
   private board: string[][];
 
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
     this.dropping = null;
+    this.position = null;
     this.board = Array.from({ length: height }, () => Array(width).fill("."));
   }
 
@@ -17,7 +19,7 @@ export class Board {
 
   drop(shape: string) {
     this.dropping = shape
-    this.board[0][1] = this.dropping
-    console.log(this.board)
+    this.position = { x: 0, y: 1 }
+    this.board[this.position.x][this.position.y] = this.dropping
   }
 }
