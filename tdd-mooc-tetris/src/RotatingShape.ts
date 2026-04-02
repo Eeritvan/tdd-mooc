f9ae80452a9e519b642c76593d484389db922a4b
eeritvan <eeritvan@proton.me>
2026-04-02 19:27:13 +0300

    part 2: progress

--- tdd-mooc-tetris/src/RotatingShape.ts
+++ tdd-mooc-tetris/src/RotatingShape.ts
@@ -13,4 +13,14 @@ export class RotatingShape {
   toString() {
     return this.grid.map(x => x.join("")).join("\n") + "\n"
   }
+
+  rotateRight() {
+    const result = Array.from({ length: this.grid.length }, () => Array(this.grid.length).fill("."))
+
+    for (let i = 0; i < this.grid.length; i++) {
+      for (let j = 0; j < this.grid[i].length; j++) {
+      }
+    }
+    return result
+  }
 }
