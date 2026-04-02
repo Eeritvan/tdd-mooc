335f5a96e8cd222b026230857bd9e58574b71155
eeritvan <eeritvan@proton.me>
2026-04-02 18:40:10 +0300

    part 2: added toString function

--- tdd-mooc-tetris/src/RotatingShape.ts
+++ tdd-mooc-tetris/src/RotatingShape.ts
@@ -2,12 +2,15 @@ export class RotatingShape {
   private grid: string[][]
 
   constructor(grid: string[][]) {
-    console.log(grid)
     this.grid = grid
   }
 
   static fromString(shape: string) {
     const grid = shape.split("\n").map(x => x.trim().split(""))
     return new RotatingShape(grid)
+  }
+
+  toString() {
+    return this.grid.map(x => x.join("")).join("\n") + "\n"
   }
 }
