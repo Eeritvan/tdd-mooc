dafc2eaf81a42a997e346500c7f88dd3db5f6b03
eeritvan <eeritvan@proton.me>
2026-04-02 20:30:57 +0300

    part 2: refactor

--- tdd-mooc-tetris/src/RotatingShape.ts
+++ tdd-mooc-tetris/src/RotatingShape.ts
@@ -19,14 +19,9 @@ export class RotatingShape {
   }
 
   rotateRight() {
-    const result = Array.from({ length: this.grid.length }, () => Array(this.grid[0].length).fill("."))
-    for (let y = 0; y < this.grid.length; y++) {
-      for (let x = 0; x < this.grid[y].length; x++) {
-        result[x][this.grid.length - 1 - y] = this.grid[y][x]
-      }
-    }
-
-    return this.formatGrid(result)
+    return this.formatGrid(
+      this.grid[0].map((_, i) => this.grid.map(row => row[i]).reverse())
+    )
   }
 
   rotateLeft() {
