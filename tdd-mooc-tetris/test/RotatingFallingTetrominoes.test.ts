9d60c39b10ff6734e5d51bf95fc696bfbe8be2fc
eeritvan <eeritvan@proton.me>
2026-04-07 21:48:46 +0300

    part 2: added failing rotate left test

---
+++ tdd-mooc-tetris/test/RotatingFallingTetrominoes.test.ts
@@ -0,0 +1,41 @@
+import { beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { Board } from "../src/Board";
+import { Tetromino } from "../src/Tetromino";
+
+describe("falling rotating tetrominoes", () => {
+  let board: Board;
+  beforeEach(() => {
+    board = new Board(10, 6);
+  });
+
+  test("a falling tetromino can be rotated left", () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.rotateLeft();
+
+    expect(board.toString()).to.equalShape(
+      `....T.....
+       ....TT....
+       ....T.....
+       ..........
+       ..........
+       ..........`
+    );
+  });
+
+  test.skip("a falling tetromino can be rotated right", () => {
+
+  });
+
+  test.skip("a falling tetromino cant be rotated left if there is no room", () => {
+
+  });
+
+  test.skip("a falling tetromino cant be rotated right if there is no room", () => {
+
+  });
+
+  test.skip("wall kick???", () => {
+
+  });
+});
