96e0c8d52eb54a0b972e78a6b90168d5d417ea2f
eeritvan <eeritvan@proton.me>
2026-04-07 15:07:16 +0300

    part 2: progress

--- tdd-mooc-tetris/src/Tetromino.ts
+++ tdd-mooc-tetris/src/Tetromino.ts
@@ -4,12 +4,16 @@ import { isFloat64Array } from "node:util/types";
 
 export class Tetromino {
   private shape: RotatingShape
+  private orientations: number
+  private currentOrientation: number
 
-  constructor(shape: RotatingShape) {
+  constructor(shape: RotatingShape, orientation: number, currentOrientation: number) {
     this.shape = shape
+    this.orientations = orientation
+    this.currentOrientation = currentOrientation
   }
 
-  static createGrid(shape: string) {
+  static createGrid(shape: string, orientations: number, currentOrientations: number) {
     const newShape = RotatingShape.fromString(shape)
     return new Tetromino(newShape)
   }
