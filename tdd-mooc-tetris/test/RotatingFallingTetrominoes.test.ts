51ee03eb9d5e45e1921e109aa56bc35213a7cd91
eeritvan <eeritvan@proton.me>
2026-04-07 23:20:18 +0300

    part 2: added failing test for rotating left with no room

--- tdd-mooc-tetris/test/RotatingFallingTetrominoes.test.ts
+++ tdd-mooc-tetris/test/RotatingFallingTetrominoes.test.ts
@@ -3,6 +3,12 @@ import { expect } from "chai";
 import { Board } from "../src/Board";
 import { Tetromino } from "../src/Tetromino";
 
+function fallToBottom(board: Board) {
+  for (let i = 0; i < 10; i++) {
+    board.moveDown();
+  }
+}
+
 describe("falling rotating tetrominoes", () => {
   let board: Board;
   beforeEach(() => {
@@ -37,8 +43,33 @@ describe("falling rotating tetrominoes", () => {
     );
   });
 
-  test.skip("a falling tetromino cant be rotated left if there is no room", () => {
+  test("a falling tetromino cant be rotated left if there is no room", () => {
+    board.drop(Tetromino.I_SHAPE);
+    board.rotateRight();
+    board.moveLeft()
+    fallToBottom(board)
 
+    board.drop(Tetromino.I_SHAPE);
+    board.rotateRight();
+    board.moveRight()
+    board.moveRight()
+    fallToBottom(board)
+
+    board.drop(Tetromino.T_SHAPE);
+    board.rotateLeft()
+    board.moveRight()
+    board.moveDown()
+    board.moveDown()
+    board.rotateLeft()
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ...I.TI...
+       ...ITTI...
+       ...I.TI...
+       ...I..I...`
+    );
   });
 
   test.skip("a falling tetromino cant be rotated right if there is no room", () => {
