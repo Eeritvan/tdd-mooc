f45f94ab58cd3f79f4eeb30806954ae7f0d3508f
Eeritvan <144259545+Eeritvan@users.noreply.github.com>
2026-04-14 15:52:12 +0300

    part 2: added S_SHAPE

--- tdd-mooc-tetris/src/Tetromino.ts
+++ tdd-mooc-tetris/src/Tetromino.ts
@@ -136,6 +136,31 @@ export class Tetromino {
     ])
   }
 
+  static get S_SHAPE() {
+    return Tetromino.createGrid([
+      [
+        ['.', '.', '.'],
+        ['.', 'S', 'S'],
+        ['S', 'S', '.']
+      ],
+      [
+        ['S', '.', '.'],
+        ['S', 'S', '.'],
+        ['.', 'S', '.']
+      ],
+      [
+        ['.', '.', '.'],
+        ['.', 'S', 'S'],
+        ['S', 'S', '.']
+      ],
+      [
+        ['S', '.', '.'],
+        ['S', 'S', '.'],
+        ['.', 'S', '.']
+      ],
+    ])
+  }
+
   toString() {
     const index = ((this.orientationIndex % 4) + 4) % 4
     return this.orientations[index].map(x => x.join("")).join('\n') + '\n'
