94fa903e19115d44adbc57a23b86aee6a192c071
eeritvan <144259545+Eeritvan@users.noreply.github.com>
2026-04-27 20:33:37 +0300

    part 2: refactoring

--- tdd-mooc-tetris/test/subscription.test.ts
+++ tdd-mooc-tetris/test/subscription.test.ts
@@ -1,35 +1,61 @@
-import { beforeEach, describe, test } from "vitest";
-import { expect } from "chai";
-import { Board } from "../src/Board";
-import { Scoring } from "../src/Scoring";
-import { Tetromino } from "../src/Tetromino";
+import { beforeEach, describe, test } from "vitest"
+import { expect } from "chai"
+import { Board } from "../src/Board"
+import { Scoring } from "../src/Scoring"
+import { Tetromino } from "../src/Tetromino"
 
 function fallToBottom(board: Board) {
   for (let i = 0; i < 10; i++) {
-    board.moveDown();
+    board.moveDown()
   }
+}
+
+function dropIOnRight(board: Board) {
+  board.drop(Tetromino.I_SHAPE)
+  board.tick()
+  board.rotateLeft()
+  board.moveRight()
+  board.moveRight()
+  fallToBottom(board)
+}
+
+function dropI(board: Board) {
+  board.drop(Tetromino.I_SHAPE)
+  fallToBottom(board)
+}
+
+function clearFourLines(board: Board) {
+  for (let i = 0; i < 4; i++) dropI(board)
+  dropIOnRight(board)
 }
 
 describe("Subscribe", () => {
   let board: Board
   let scoring: Scoring
   beforeEach(() => {
-    board = new Board(5, 8);
-    scoring = new Scoring();
-    board.subscribe(scoring);
-  });
+    board = new Board(5, 8)
+    scoring = new Scoring()
+    board.subscribe(scoring)
+  })
 
   test("clearing 1 line on board rewards 40 points", () => {
-    board.drop(Tetromino.I_SHAPE)
-    fallToBottom(board)
-
-    board.drop(Tetromino.I_SHAPE)
-    board.tick()
-    board.rotateLeft()
-    board.moveRight()
-    board.moveRight()
-    fallToBottom(board)
+    dropI(board)
+    dropIOnRight(board)
 
     expect(scoring.score).to.equal(40)
-  });
-});
+  })
+
+  test("clearing 4 line on board rewards 1200 points", () => {
+    clearFourLines(board)
+
+    expect(scoring.score).to.equal(1200)
+  })
+
+  test("clearing 10 line on board increases the level", () => {
+    clearFourLines(board)
+    clearFourLines(board)
+    clearFourLines(board)
+
+    expect(scoring.level).to.equal(1)
+  })
+})
