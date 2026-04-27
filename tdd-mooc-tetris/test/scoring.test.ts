c8c202d269feed45aa5ec9c035591dbdd4875188
eeritvan <144259545+Eeritvan@users.noreply.github.com>
2026-04-27 17:46:57 +0300

    part 2: added failing test for scoring system

---
+++ tdd-mooc-tetris/test/scoring.test.ts
@@ -0,0 +1,15 @@
+import { beforeEach, describe, test } from "vitest"
+import { expect } from "chai"
+import { Scoring } from "../src/Scoring"
+
+describe("scoring", () => {
+  let scoring: Scoring
+  beforeEach(() => {
+    scoring = new Scoring()
+  })
+
+  test("clearing 1 line rewards 40 points", () => {
+    scoring.clearLine(1)
+    expect(scoring.score).to.equalShape(40)
+  })
+})
