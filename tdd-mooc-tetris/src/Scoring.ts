a0ff65294d421272a554ca0d11138a27b4b12a9d
eeritvan <144259545+Eeritvan@users.noreply.github.com>
2026-04-27 17:56:25 +0300

    part 2: progress

--- tdd-mooc-tetris/src/Scoring.ts
+++ tdd-mooc-tetris/src/Scoring.ts
@@ -2,10 +2,24 @@ export class Scoring {
   score: number
 
   constructor() {
-    this.score = 40
+    this.score = 0
   }
 
   clearLine(lines: number) {
+    switch (lines) {
+      case 1:
+        this.score += 40
+        break
+      case 2:
+        this.score += 100
+        break
+      case 3:
+        this.score += 300
+        break
+      case 4:
+        this.score += 1200
+        break
+    }
     return
   }
 }
