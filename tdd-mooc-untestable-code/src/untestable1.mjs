a5c19170b305ae0b4f6b7df89c83db5577ec0c38
eeritvan <144259545+Eeritvan@users.noreply.github.com>
2026-04-29 23:38:00 +0300

    part 3: task 1

--- tdd-mooc-untestable-code/src/untestable1.mjs
+++ tdd-mooc-untestable-code/src/untestable1.mjs
@@ -1,7 +1,7 @@
+// challenge: time
 const millisPerDay = 24 * 60 * 60 * 1000;
 
-export function daysUntilChristmas() {
-  const now = new Date();
+export function daysUntilChristmas(now = new Date()) {
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
   const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
   if (today.getTime() > christmasDay.getTime()) {
@@ -10,3 +10,15 @@ export function daysUntilChristmas() {
   const diffMillis = christmasDay.getTime() - today.getTime();
   return Math.floor(diffMillis / millisPerDay);
 }
+
+// OG CODE
+// export function daysUntilChristmas() {
+//   const now = new Date();
+//   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+//   const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
+//   if (today.getTime() > christmasDay.getTime()) {
+//     christmasDay.setFullYear(new Date().getFullYear() + 1);
+//   }
+//   const diffMillis = christmasDay.getTime() - today.getTime();
+//   return Math.floor(diffMillis / millisPerDay);
+// }
--- tdd-mooc-untestable-code/test/untestable1.test.mjs
+++ tdd-mooc-untestable-code/test/untestable1.test.mjs
@@ -3,8 +3,19 @@ import { expect } from "chai";
 import { daysUntilChristmas } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
-  test("todo", () => {
-    // TODO: write proper tests
+  test("is a number", () => {
     expect(daysUntilChristmas()).to.be.a("number");
+  });
+
+  test("is 0 on christmas day", () => {
+    const currentYear = new Date().getFullYear()
+    const time = new Date(currentYear, 11, 25)
+    expect(daysUntilChristmas(time)).to.be.equal(0);
+  });
+
+  test("is 30 a month before christmas", () => {
+    const currentYear = new Date().getFullYear()
+    const time = new Date(currentYear, 10, 25)
+    expect(daysUntilChristmas(time)).to.be.equal(30);
   });
 });
